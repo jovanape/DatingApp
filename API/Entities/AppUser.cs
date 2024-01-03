9faf3e5fcfa7036c3c58e663d8cbbfb3ad323f13
Pejkic Jovana TH-BEL4 <Jovana.Pejkic@draexlmaier.com>
2024-01-03 13:34:04 +0100

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    // moramo jedno polje da nazovemo Id ili da dodamo [Key] iznad - Entity ga koristi kao PK u bazi
+    public int Id { get; set; } // Entity framework needs it to be public - get and set
+    public string UserName { get; set; } // C# is case sensitive
+
+}
