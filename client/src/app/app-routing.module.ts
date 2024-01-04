660710ad545f1bd806a75fd58d24c2a1d786554f
Pejkic Jovana TH-BEL4 <Jovana.Pejkic@draexlmaier.com>
2024-01-04 18:32:44 +0100

    End of section 3

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+//import { Routes } from '@angular/router';
+
+//export const routes: Routes = [];
+
+
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
